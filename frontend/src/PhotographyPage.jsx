152ba93ad83394b5b2b83911b0bfa5a4f70d7aed
Urvi Patel <upatel@ualberta.ca>
2022-08-15 18:37:11 -0600

    finished photography selection page

--- frontend/src/PhotographyPage.jsx
+++ frontend/src/PhotographyPage.jsx
@@ -1,16 +1,69 @@
-// import { Button } from '@mui/material';
+import { ImageListItem, ImageListItemBar } from '@mui/material';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Styles.css';
 
 function PhotographyPage() {
   const navigate = useNavigate();
-  const navigateToLink = () => {
+  const navigateToLink = (link) => {
     window.scrollTo(0, 0);
-    navigate('/landscape-photography');
+    navigate(link);
   };
+  const landscapeImage = {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: 'Landscape Photography',
+  };
+
+  const portraitImage = {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    title: 'Portrait Photography',
+  };
+
   return (
     <div className="Photography">
-      <button type="button" onClick={navigateToLink}>Choose type of photography</button>
+      <StyledEngineProvider injectFirst>
+
+        <div className="photoTypes">
+          <ImageListItem
+            className="type"
+            key={landscapeImage.img}
+            onClick={() => { navigateToLink('/landscape-photography'); }}
+          >
+            <img
+              src={`${landscapeImage.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${landscapeImage.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={landscapeImage.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={landscapeImage.title}
+              subtitle={landscapeImage.location}
+            />
+          </ImageListItem>
+          <ImageListItem
+            className="type"
+            key={portraitImage.img}
+            onClick={() => { navigateToLink('/portrait-photography'); }}
+          >
+            <img
+              src={`${portraitImage.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${portraitImage.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={portraitImage.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={portraitImage.title}
+            />
+          </ImageListItem>
+        </div>
+        <div className="aboutDiv">
+          <h1>
+            {/* <NavLink to="/" className="siteName">Captured in Firelight</NavLink> */}
+            <NavLink to="/about" className="aboutButton">About</NavLink>
+          </h1>
+        </div>
+      </StyledEngineProvider>
     </div>
   );
 }
