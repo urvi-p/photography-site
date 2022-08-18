f3ba19a66e5e8d5eafb8efbcfc4b5e252805e973
Urvi Patel <upatel@ualberta.ca>
2022-08-18 11:39:35 -0600

    initialized firebase

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import FirebaseProvider from '../utils/firebase';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <FirebaseProvider>
+    <Router>
+      <App />
+    </Router>
+  </FirebaseProvider>,
   document.getElementById('root'),
 );
